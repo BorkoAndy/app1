0272aa95c8a172b97abb6fc2bea436a0ad3ee53c
BorkoAndrii <1983andy2016@gmail.com>
2024-11-27 13:22:10 +0100

    Index and about templates are made

--- main/views.py
+++ main/views.py
@@ -2,15 +2,8 @@ from django.http import HttpResponse, QueryDict
 from django.shortcuts import render
 
 
-def index(request):
-    context = {
-        'title': 'Home',
-        'content': 'Main HOME page',
-        'list': ['first', 'second'],
-        'dict': {'first': 1},
-        "is_authenticated": True
-    }    
-    return render(request, 'main/index.html', context)
+def index(request):    
+    return render(request, 'main/index.html')
 
 def about(request):
-    return HttpResponse("About page")
+    return render(request, 'main/about.html')
