791619ffaaa845b0bc4855dd38726ee6f91f6dc0
BorkoAndrii <1983andy2016@gmail.com>
2024-11-28 16:29:49 +0100

    Added fixtures for goods and products

--- goods/models.py
+++ goods/models.py
@@ -1,3 +1,26 @@
+from pyexpat import model
 from django.db import models
 
 # Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=150, unique=True)
+    slug = models.SlugField(max_length=200, unique=True, blank=True, null=True)
+
+    def __str__(self):
+        return f"{self.name}"
+    
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=150, unique=True)
+    slug = models.SlugField(max_length=200, unique=True, blank=True, null=True)
+    description = models.TextField(blank=True, null=True)
+    image = models.ImageField(upload_to='goods_images', blank=True, null=True)
+    price = models.DecimalField(default=0.00, max_digits=7, decimal_places=2)
+    discount = models.DecimalField(default=0.00, max_digits=7, decimal_places=2)
+    quantity = models.PositiveIntegerField(default=0)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.name} {self.quantity}"
--- main/views.py
+++ main/views.py
@@ -1,9 +1,15 @@
 from django.http import HttpResponse, QueryDict
 from django.shortcuts import render
 
+from goods.models import Category
 
-def index(request):    
-    return render(request, 'main/index.html')
+
+def index(request): 
+    categories = Category.objects.all()   
+    return render(request, 'main/index.html', {
+        'categories': categories
+    })
+                  
 
 def about(request):
     return render(request, 'main/about.html')
