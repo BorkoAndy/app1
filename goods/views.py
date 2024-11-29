8d4cd37c93e23b0273e89c91feb94b20010d534f
BorkoAndrii <1983andy2016@gmail.com>
2024-11-29 15:08:56 +0100

    Before pagination

--- goods/views.py
+++ goods/views.py
@@ -1,13 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import get_list_or_404, render
 
-from goods.models import Product
+from goods.models import Product, Category
 
-def catalog(request):
-    goods = Product.objects.all()
+def catalog(request, category_slug):
+    if category_slug == 'all':
+        goods = Product.objects.all()
+    else:
+         goods = get_list_or_404(Product.objects.filter(category__slug=category_slug))
+    
     context = {   
         'goods': goods,
         }
     return render(request, "goods/catalog.html", context)
 
-def product(request):
-    return render(request, "goods/product.html")
+def product(request, product_slug):
+    product = Product.objects.get(slug=product_slug)
+    context = {
+        'product':  product,
+        }
+    return render(request, "goods/product.html", context=context)
+
+
