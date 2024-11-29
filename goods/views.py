4b734636d2d3d2784295f9c8415b41e7d2c83257
BorkoAndrii <1983andy2016@gmail.com>
2024-11-29 17:14:00 +0100

    error catched during making a form

--- goods/views.py
+++ goods/views.py
@@ -8,15 +8,27 @@ def catalog(request, category_slug):
         goods = Product.objects.all()
     else:
          goods = get_list_or_404(Product.objects.filter(category__slug=category_slug))
+       
+    on_sale = request.GET.get('on_sale', None)
+    order_by = request.GET.get('order_by', None)
+
+    if on_sale:
+        goods = goods.filter(discount__gt=0)
+    if order_by and order_by != 'default':
+        goods = goods.order_by(order_by)
 
     paginator = Paginator(goods, 6)
     page_number = request.GET.get("page")
     page_obj = paginator.get_page(page_number)    
-    
+
+
     context = {   
         'goods': page_obj,
         }
     return render(request, "goods/catalog.html", context)
+
+
+
 
 def product(request, product_slug):
     product = Product.objects.get(slug=product_slug)
