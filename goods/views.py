da3582c0a317addf7b9646b91452defa9e38b670
BorkoAndrii <1983andy2016@gmail.com>
2024-11-30 19:11:12 +0100

    Registration and logging in are ready

--- goods/views.py
+++ goods/views.py
@@ -2,28 +2,40 @@ from django.shortcuts import get_list_or_404, render
 from django.core.paginator import Paginator
 
 from goods.models import Product, Category
+from goods.utils import q_search
 
-def catalog(request, category_slug):
-    if category_slug == 'all':
-        goods = Product.objects.all()
-    else:
-         goods = get_list_or_404(Product.objects.filter(category__slug=category_slug))
-       
+def catalog(request, category_slug=None):
+    # Filter data  
     on_sale = request.GET.get('on_sale', None)
     order_by = request.GET.get('order_by', None)
 
+    # Search data
+    query = request.GET.get('q', None)
+
+    # Checking url query
+    if category_slug == 'all':
+        goods = Product.objects.all()
+    elif query:
+        goods = q_search(query)
+    else:
+         goods = (Product.objects.filter(category__slug=category_slug))   
+
+    # Checking filters
     if on_sale:
         goods = goods.filter(discount__gt=0)
     if order_by and order_by != 'default':
         goods = goods.order_by(order_by)
 
+    # Pagination
     paginator = Paginator(goods, 6)
     page_number = request.GET.get("page")
-    page_obj = paginator.get_page(page_number)    
+    page_obj = paginator.get_page(page_number)
 
 
+    # Context formation
     context = {   
         'goods': page_obj,
+        # 'slug_url': category_slug
         }
     return render(request, "goods/catalog.html", context)
 
---
+++ users/forms.py
@@ -0,0 +1,40 @@
+from django import forms
+from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
+from users.models import User
+
+class UserLoginForm(AuthenticationForm):
+    username = forms.CharField(widget=forms.TextInput(attrs={
+        "autofocus": True,
+        'class': 'form-control',
+        'placeholder': "Input username"
+        }))
+    password = forms.CharField(widget=forms.PasswordInput(attrs={
+            "autocomplete": "current-password",
+            'class': "form-control",
+            'placeholder': "Input password",
+            }),
+    )
+    class Meta:
+        model = User
+        fields = ['username', 'password',] 
+
+
+class UserRegistrationForm(UserCreationForm):
+
+    class Meta:
+        model = User
+        fields = {
+            "first_name",
+            "last_name",
+            "username",
+            "password1",
+            "password2",
+        }
+
+    first_name = forms.CharField()        
+    last_name = forms.CharField()
+    username = forms.CharField()
+    email = forms.CharField()
+    password1 = forms.CharField()        
+    password2 = forms.CharField()
+        
