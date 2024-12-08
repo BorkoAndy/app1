63691f293006128a6c35c0fdc1c9fd01e940c729
BorkoAndrii <1983andy2016@gmail.com>
2024-12-08 08:58:42 +0100

    Last before docker

--- orders/forms.py
+++ orders/forms.py
@@ -17,6 +17,28 @@ class CreateOrderForm(forms.Form):
                 ("0", False),
                 ("1", True),
         ],)
+<<<<<<< HEAD
+=======
+
+
+    def clean_phone_number(self):
+        data = self.cleaned_data['phone_number']
+
+        if not data.isdigit():
+            raise forms.ValidationError("Phone number should contain digits only")
+        return data
+
+
+
+    # first_name = forms.CharField(
+    #     widget=forms.TextInput(
+    #         attrs={
+    #             "class": "form-control",
+    #             "placeholder": "Enter your first name",
+    #         }
+    #     )
+    # )
+>>>>>>> 57ae5a3 (Last before docker)
 
 
     def clean_phone_number(self):
